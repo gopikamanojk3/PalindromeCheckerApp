82b0758a3a3dcf92bd4d7f50b49313c91630ef2b
gopikamanojk3 <gopika.manojk3@gmail.com>
2026-03-16 08:46:56 +0530

    UC10

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,30 +1,44 @@
+
+DIYA WADHWANI (RA2411003010039)
+        8:23 AM (18 minutes ago)
+
+        Add reaction
+
+        Reply
+
+        More
+        to PARNAVI, me, ADITYA
+
 import java.util.*;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "madam";
+
+        String input = "A man a plan a canal Panama";
+
+
+        input = input.replaceAll("\\s+", "").toLowerCase();
 
         boolean result = check(input, 0, input.length() - 1);
 
-        System.out.println(" Input : " + input);
+        System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + result);
     }
 
-    // Recursive method
+
     private static boolean check(String s, int start, int end) {
 
-        // Base condition
+
         if (start >= end) {
             return true;
         }
 
-        // If mismatch found
+
         if (s.charAt(start) != s.charAt(end)) {
             return false;
         }
 
-        // Recursive call
+
         return check(s, start + 1, end - 1);
     }
 }
-
-
